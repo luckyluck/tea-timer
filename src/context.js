57b17878e566ced156f35759bbe3c690f2164361
Alexandr Ketov <ketov.alex.v@gmail.com>
2019-06-01 22:52:12 +0200

    Replace CRA with React+Webpack app.

--- src/context.js
+++ src/context.js
@@ -6,14 +6,15 @@ type DefaultContext = {
   periods: Array<number>,
 };
 
-const periods = process.env.REACT_APP_PERIODS ? process.env.REACT_APP_PERIODS.split(' ').map(i => +i) : [
-  30000,  // 30 sec
-  180000, // 3 min
-  210000, // 3 min 30 sec
-  240000, // 4 min
-  270000, // 4 min 30 sec
-  300000 // 5 min
-];
+const periods = process.env.NODE_ENV !== 'production' && process.env.TIME_PERIODS ?
+  process.env.TIME_PERIODS.split(' ').map(i => +i) : [
+    30000,  // 30 sec
+    180000, // 3 min
+    210000, // 3 min 30 sec
+    240000, // 4 min
+    270000, // 4 min 30 sec
+    300000 // 5 min
+  ];
 
 export const defaultContext: DefaultContext = {
   limit: 5,
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+  template: './public/index.html',
+  filename: './index.html'
+});
+
+module.exports = {
+  output: {
+    path: __dirname + '/build'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    htmlWebpackPlugin,
+    new Dotenv({
+      path: process.env.NODE_ENV !== 'production' ? './.env.development' : './.env.production', // Path to .env.development file (this is the default)
+      safe: false, // load .env.development.example (defaults to "false" which does not use dotenv-safe)
+    })
+  ]
+};
