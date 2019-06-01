e99891508b393f164666367532c689178a9867b7
Alexandr Ketov <ketov.alex.v@gmail.com>
2019-06-01 16:56:08 +0200

    Updates

---
+++ precache-manifest.2b8064fe8bf4a867a80e8ce485766168.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae42f4862a0691adc5a0ad30abf5ba1d",
+    "url": "/tea-timer/index.html"
+  },
+  {
+    "revision": "75f40316290f7ca43e9b",
+    "url": "/tea-timer/static/css/main.0629f81c.chunk.css"
+  },
+  {
+    "revision": "41cd9383378aca997531",
+    "url": "/tea-timer/static/js/2.8d4b9b98.chunk.js"
+  },
+  {
+    "revision": "75f40316290f7ca43e9b",
+    "url": "/tea-timer/static/js/main.dcb5ea34.chunk.js"
+  },
+  {
+    "revision": "204ea993b8430a24e251",
+    "url": "/tea-timer/static/js/runtime~main.50f6573e.js"
+  }
+]);
