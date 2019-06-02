037989d87dc4246ecd43a64ef3727dd88e71808d
Alexandr Ketov <ketov.alex.v@gmail.com>
2019-06-02 16:31:10 +0200

    Updates

---
+++ precache-manifest.6f11ecc5474dec0f669d69a0ada343c1.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99dcbf6e3a9e2265aa13bce99e25ef51",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e54040443f83c69462ff",
+    "url": "main.js"
+  }
+]);
