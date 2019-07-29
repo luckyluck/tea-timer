ce396b01c14a66fe7338c67777fb7ef0e51c9a4b
Alexandr Ketov <ketov.alex.v@gmail.com>
2019-07-29 21:10:21 +0200

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,21 +1,19 @@
 const CACHE = 'network-update-cache';
 const STATIC_FILES = [
-  '/',
-  '/index.html',
-  '/favicon.ico',
-  '/manifest.json',
-  '/assets/nosleep.mov',
-  '/images/app-icon-48x48.png',
-  '/images/app-icon-72x72.png',
-  '/images/app-icon-76x76.png',
-  '/images/app-icon-96x96.png',
-  '/images/app-icon-120x120.png',
-  '/images/app-icon-144x144.png',
-  '/images/app-icon-152x152.png',
-  '/images/app-icon-180x180.png',
-  '/images/app-icon-192x192.png',
-  '/images/app-icon-256x256.png',
-  '/images/app-icon-512x512.png',
+  'index.html',
+  'favicon.ico',
+  'assets/nosleep.mov',
+  'images/app-icon-48x48.png',
+  'images/app-icon-72x72.png',
+  'images/app-icon-76x76.png',
+  'images/app-icon-96x96.png',
+  'images/app-icon-120x120.png',
+  'images/app-icon-144x144.png',
+  'images/app-icon-152x152.png',
+  'images/app-icon-180x180.png',
+  'images/app-icon-192x192.png',
+  'images/app-icon-256x256.png',
+  'images/app-icon-512x512.png',
 ];
 
 self.addEventListener('install', event => {
@@ -33,15 +31,7 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   console.log('[Service Worker] Serving the asset...');
   // Cache with network and update fallback strategy
-  event.respondWith(function() {
-    try {
-      console.log('--------->event.request:', event.request);
-      return fromCache(event.request);
-    } catch (err) {
-      console.log('--------->event.request:', event.request);
-      return fromNetworkWithUpdate(event.request);
-    }
-  }());
+  event.respondWith((() => fromCache(event.request))());
 });
 
 async function preCache() {
@@ -51,15 +41,15 @@ async function preCache() {
 
 async function fromCache(request) {
   const cache = await caches.open(CACHE);
-  const matching = cache.match(request);
-  return matching || Promise.reject('no-match');
+  const matching = await cache.match(request);
+  return matching || await fromNetworkWithUpdate(request);
 }
 
 async function fromNetworkWithUpdate(request) {
   const cache = await caches.open(CACHE);
+  // Requesting data
   const response = await fetch(request);
-  console.log('--------->request:', request);
+  // Dynamically caching requested data
   cache.put(request, response.clone());
-  console.log('--------->response:', response.clone());
   return response;
 }
